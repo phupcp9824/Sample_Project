76c82097acb087207c363fbed3c388282c1725d5
phupcp9824 <phamphu872004@gmail.com>
2024-09-09 22:02:46 +0700

    Add project files.

---
+++ Interface_Sell/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Interface_Sell.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
